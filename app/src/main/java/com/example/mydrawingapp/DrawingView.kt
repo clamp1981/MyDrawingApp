cbbc53b8ec9519b24aad598698d40c91a8d3fbba
clamp1981 <clamp1981@gmail.com>
2022-06-01 18:02:24 +0900

    feat : add setSizeBrush

--- app/src/main/java/com/example/mydrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/mydrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.mydrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -30,8 +31,14 @@ class DrawingView( context : Context, attributeSet : AttributeSet ) : View( cont
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mDrawingPath = CustomPath(color,mBrushSize)
         mCanvasPaint = Paint( Paint.DITHER_FLAG )
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
 
+    }
+
+    public fun setSizeBrush( newSize : Float ){
+        mBrushSize = TypedValue.applyDimension( TypedValue.COMPLEX_UNIT_DIP,
+                                                newSize, resources.displayMetrics )
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
     //view 사이즈가 변경 될 때 마다 Canvas를불러옴
