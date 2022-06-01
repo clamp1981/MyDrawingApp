30e202a9ea345eaa2285a17c7a9e749253d2b000
clamp1981 <clamp1981@gmail.com>
2022-06-01 16:12:38 +0900

    feat : add Drawing line

--- app/src/main/java/com/example/mydrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/mydrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.mydrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class DrawingView( context : Context, attributeSet : AttributeSet ) : View( context, attributeSet ) {
@@ -14,10 +15,12 @@ class DrawingView( context : Context, attributeSet : AttributeSet ) : View( cont
     private var mBrushSize : Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas : Canvas? = null
+    private var mDrawPaths = ArrayList<CustomPath>()
 
     init{
         setUpDrawing()
     }
+
 
     private fun setUpDrawing() {
         mDrawPaint = Paint()
@@ -29,6 +32,61 @@ class DrawingView( context : Context, attributeSet : AttributeSet ) : View( cont
         mCanvasPaint = Paint( Paint.DITHER_FLAG )
         mBrushSize = 20.toFloat()
 
+    }
+
+    //view 사이즈가 변경 될 때 마다 Canvas를불러옴
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap( w, h, Bitmap.Config.ARGB_8888 )
+        canvas = Canvas( mCanvasBitmap!!)
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap( mCanvasBitmap!!, 0f,0f, mCanvasPaint )
+
+        for( path in mDrawPaths ){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas?.drawPath( path, mDrawPaint!! )
+        }
+        if( !mDrawingPath!!.isEmpty ){
+            mDrawPaint!!.strokeWidth = mDrawingPath!!.brushThickness
+            mDrawPaint!!.color = mDrawingPath!!.color
+            canvas?.drawPath( mDrawingPath!!, mDrawPaint!! )
+        }
+
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+        when( event?.action ){
+            MotionEvent.ACTION_DOWN -> {
+                mDrawingPath!!.color = color
+                mDrawingPath!!.brushThickness = mBrushSize
+
+                mDrawingPath!!.reset()
+                if( touchX != null && touchY != null )
+                    mDrawingPath!!.moveTo( touchX, touchY )
+            }
+            MotionEvent.ACTION_MOVE ->{
+                if( touchX != null && touchY != null )
+                    mDrawingPath!!.lineTo( touchX, touchY )
+            }
+
+            MotionEvent.ACTION_UP ->{
+                mDrawPaths.add(mDrawingPath!!)
+                mDrawingPath = CustomPath( color, mBrushSize )
+            }
+            else -> return false
+
+        }
+        invalidate()
+        
+        return true
     }
 
     internal inner class CustomPath( var color : Int, var brushThickness : Float ) : Path(){
